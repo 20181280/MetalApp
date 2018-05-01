fb897f6d336e999c25f9e1ad88490b96ffc2b464
Dormer Beau <20181280@co-314-14.local>
2018-05-01 13:20:53 -0400

    Initial Commit

---
+++ dormer_metalAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dormer_metalAPP
+//
+//  Created by Dormer Beau on 5/1/18.
+//  Copyright © 2018 Dormer Beau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
